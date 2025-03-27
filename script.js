4720ec3b4768c5d680b5f67ba1a4f4c98702bcfc
Qutef <mrqute.f@gmail.com>
2025-03-27 08:06:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,17 +59,6 @@ function loadLesson(categoryId, lessonId) {
 
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-mode");
-    }
-});
-
-document.getElementById("theme-toggle").addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
-    localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
-});
-
 function markAsCompleted() {
     const title = document.getElementById("lesson-title").textContent;
     
